406d5b0a5a8c262cb9d71f407214ab6ea0157234
gianfava <gianfava@gmail.com>
2025-05-27 20:49:56 -0300

    Adicionado mobile

---
+++ mobile/CardioCheck/LoginPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace CardioCheck;
+
+public partial class LoginPage : ContentPage
+{
+    public LoginPage()
+    {
+        InitializeComponent();
+        NavigationPage.SetHasNavigationBar(this, false);
+    }
+
+    private void OnEntrarClicked(object sender, EventArgs e)
+    {
+        string usuario = UsuarioEntry.Text?.Trim();
+        string senha = SenhaEntry.Text?.Trim();
+
+        if (usuario == "admin" && senha == "admin")
+        {
+
+            Microsoft.Maui.Controls.Application.Current.Dispatcher.Dispatch(() =>
+            {
+                Application.Current.MainPage = new MenuPrincipal();
+            });
+        }
+        else
+        {
+            MensagemErro.Text = " Usu�rio ou senha incorretos! \nTente novamente.";
+        }
+    }
+
+}
---
+++ mobile/CardioCheck/MenuPrincipal.xaml.cs
@@ -0,0 +1,46 @@
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.Controls.PlatformConfiguration.AndroidSpecific;
+
+namespace CardioCheck;
+
+public partial class MenuPrincipal : Microsoft.Maui.Controls.TabbedPage
+{
+    public MenuPrincipal()
+    {
+        InitializeComponent();
+        NavigationPage.SetHasNavigationBar(this, false);
+        On<Microsoft.Maui.Controls.PlatformConfiguration.Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
+    }
+
+
+    private void CalcularRisco_Clicked(object sender, EventArgs e)
+    {
+        bool idadeValida = int.TryParse("0", out int idade);
+        bool pressaoValida = int.TryParse(PressaoEntry.Text, out int pressao);
+        bool colesterolValido = int.TryParse(ColesterolEntry.Text, out int colesterol);
+
+        if (!idadeValida || !pressaoValida || !colesterolValido)
+        {
+            ResultadoLabel.Text = "Por favor, preencha todos os campos corretamente.";
+            ResultadoLabel.TextColor = Colors.Red;
+            return;
+        }
+
+        int score = 0;
+        if (idade > 50) score += 2;
+        if (pressao > 130) score += 2;
+        if (colesterol > 200) score += 2;
+
+        string risco = score switch
+        {
+            0 => "Baixo risco card aco",
+            2 => "Risco moderado",
+            >= 4 => "Alto risco card aco",
+            _ => "Risco indefinido"
+        };
+
+        ResultadoLabel.Text = $"Resultado: {risco}";
+        ResultadoLabel.TextColor = score >= 4 ? Colors.Red : Colors.Green;
+    }
+}
